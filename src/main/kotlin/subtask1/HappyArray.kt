f33ff67c3a389741dc3c422b96a37161fd3baa79
Ziban89 <ziban89@tut.by>
2021-05-26 16:31:06 +0300

    feat: solve first subtask

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,20 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        if (sadArray.size <= 2) return sadArray
+        val tempArr = IntArray(sadArray.size)
+        var count = 1
+        tempArr[0] = sadArray[0]
+        for (i in 1..sadArray.lastIndex - 1) {
+            if (sadArray[i] < sadArray[i - 1] + sadArray[i + 1]) {
+                tempArr[count] = sadArray[i]
+                count++
+            }
+        }
+        tempArr[count] = sadArray[sadArray.lastIndex]
+        val result = tempArr.copyOfRange(0, count + 1)
+        if (result.size == sadArray.size) return result
+        else return convertToHappy(result)
     }
 }

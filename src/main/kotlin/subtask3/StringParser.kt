763ca29d17b2193741f8c2eea7af792b6cb291c8
Ziban89 <ziban89@tut.by>
2021-05-26 16:34:01 +0300

    feat: solve third subtask

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -1,9 +1,35 @@
 package subtask3
 
+
 class StringParser {
 
-    // TODO: Complete the following function
+
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val signsMap = mapOf('<' to '>', '(' to ')', '[' to ']')
+        val resultList = mutableListOf<String>()
+        for (i in inputString.indices) {
+            if (inputString[i] in signsMap.keys) {
+                var count = 1
+                val openBracket = inputString[i]
+                for (j in i + 1..inputString.lastIndex) {
+                    if (inputString[j] == openBracket) {
+                        count++
+                    }
+                    if (inputString[j] == signsMap[openBracket]) {
+                        count--
+                        if (count == 0) {
+                            resultList.add(inputString.substring(i + 1, j))
+                            break
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+
+        return resultList.toTypedArray()
     }
 }
+
